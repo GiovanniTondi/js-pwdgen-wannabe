a57c6c466446463c646fb98c8fbbff6eab3f6d3a
Giovanni <tondigiovanni@gmail.com>
2020-06-12 15:07:41 +0200

    Aggiunto pwd generator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 // Chiedere il nome
-
+var name = prompt("Inserisci il tuo nome:");
 
 // Chiedere il cognome
-
+var surname = prompt("Inserisci il tuo cognome:");
 
 // Chiedere il colore preferito
-
+var color = prompt("Inserisci il tuo colore preferito");
 
 // Concatenare variabili
-
+var pwd = name + surname + color + '19';
 
 // Stampa pwd
+document.getElementById('pwd').innerHTML = pwd;
